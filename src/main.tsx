e178bc05f3f642a1e1af4e3a362ba0b882882cc1
zouhao08 <zouhao08@meituan.com>
2026-07-19 16:09:40 +0800

    Fix router base path on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+const routerBasename = new URL(import.meta.env.BASE_URL, window.location.href).pathname
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
